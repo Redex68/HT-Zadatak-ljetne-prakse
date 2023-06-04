fffdfd96cb75b94e4cecc4a1642fc232ff4d5d79
Redex <marin.petric73@gmail.com>
2023-06-04 16:08:00 +0200

    Initial commit

---
+++ src/test/java/hr/ht/marin/zadatak/ZadatakApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.ht.marin.zadatak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadatakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
