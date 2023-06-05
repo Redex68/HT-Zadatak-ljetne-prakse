bcd727252766764314bf63850660bd96ca6f6884
Redex <marin.petric73@gmail.com>
2023-06-05 23:11:14 +0200

    Added javadoc

--- src/main/java/hr/ht/marin/zadatak/dao/DeliveryRepository.java
+++ src/main/java/hr/ht/marin/zadatak/dao/DeliveryRepository.java
@@ -12,14 +12,35 @@ import hr.ht.marin.zadatak.entitiy.Delivery;
 import hr.ht.marin.zadatak.entitiy.DeliveryStatus;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
+    /**
+     * Fetches a list of all the deliveries whose current status matches the provided {@code status}.
+     * @param status The status of the deliveries.
+     * @return A set of deliveries. If there are none, returns an empty set.
+     */
     Set<Delivery> findByStatus(DeliveryStatus status);
 
+    /**
+     * Fetches all the deliveries created in the specified timeframe.
+     * @param start The beginning of the timeframe. Inclusive.
+     * @param end The end of the timeframe. Inclusive.
+     * @return A set of deliveries. If there are none, returns an empty set.
+     */
     @Query("SELECT d FROM Delivery d WHERE d.orderCreationTime >= :start AND d.orderCreationTime <= :end")
     Set<Delivery> findByInterval(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
+    /**
+     * Fetches all the deliveries created after the specified date.
+     * @param start The beginning of the timeframe. Inclusive.
+     * @return A set of deliveries. If there are none, returns an empty set.
+     */
     @Query("SELECT d FROM Delivery d WHERE d.orderCreationTime >= :start")
     Set<Delivery> findByIntervalStart(@Param("start") LocalDateTime start);
 
+    /**
+     * Fetches all the deliveries created before the specified date.
+     * @param end The end of the timeframe. Inclusive.
+     * @return A set of deliveries. If there are none, returns an empty set.
+     */
     @Query("SELECT d FROM Delivery d WHERE d.orderCreationTime <= :end")
     Set<Delivery> findByIntervalEnd(@Param("end") LocalDateTime end);
 }
--- src/main/java/hr/ht/marin/zadatak/dao/PhoneRepository.java
+++ src/main/java/hr/ht/marin/zadatak/dao/PhoneRepository.java
@@ -9,6 +9,12 @@ import org.springframework.data.repository.query.Param;
 import hr.ht.marin.zadatak.entitiy.Phone;
 
 public interface PhoneRepository extends JpaRepository<Phone, Long> {
+    /**
+     * Returns a phone from the specified manufacturer with the given model name.
+     * @param manufacturer The name of the manufacturer.
+     * @param modelName The name of the phone's model.
+     * @return A Phone object.
+     */
     @Query("SELECT p FROM Phone p WHERE p.manufacturer = :manufacturer AND p.modelName = :model")
     Optional<Phone> findByName(@Param("manufacturer") String manufacturer, @Param("model") String modelName);
 }
